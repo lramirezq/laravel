e15e83b47083d7fa7068cda25f22097bee6651f3
lramirez <lramirez@3htp.com>
2024-04-25 11:24:41 -0400

    fix download XML

--- app/Console/Commands/DownloadXML.php
+++ app/Console/Commands/DownloadXML.php
@@ -39,7 +39,7 @@ class DownloadXML extends Command
         Log::info("A descargar [" .$documents->count()."] XML");
         foreach ($documents as $document) {
             Log::debug("Descargando Documento XML: [" . $document->Number . "]");
-            $vv = Document::find($document->id)->path_pdf;
+            $vv = Document::find($document->id)->path_xml;
             if ($vv != null){
                 Log::info("Otro proceso ya descargo el XML[". $document->Number."]");
                 continue;
