b012a3f4b1210fe15643bb28a0e241496bc06d91
lramirez <lramirez@3htp.com>
2024-04-26 00:25:28 -0400

    solo 1 conexión

--- app/Console/Commands/CopyToSFTP.php
+++ app/Console/Commands/CopyToSFTP.php
@@ -5,13 +5,8 @@ namespace App\Console\Commands;
 use App\Models\Document;
 use Illuminate\Console\Command;
 use Illuminate\Support\Carbon;
-use App\Models\Mantenedor;
-use Illuminate\Support\Facades\Log;
 use App\Models\Evento;
-use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Storage;
-
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Filesystem\Filesystem;
 use phpseclib3\Net\SFTP;
 
@@ -29,7 +24,7 @@ class CopyToSFTP extends Command
      *
      * @var string
      */
-    protected $description = 'This function allow copy docs to sftp server';
+    protected $description = 'This function allows copying docs to an SFTP server';
 
     /**
      * Execute the console command.
@@ -38,104 +33,88 @@ class CopyToSFTP extends Command
     {
         Log::info('------------Copy to SFTP -------------------');
 
-        //buscar todos los pendientes de pasar por SFTP
+        // Establecer la conexión SFTP fuera del bucle
+        $sftp = $this->establishSFTPConnection();
+
+        // Buscar todos los pendientes de pasar por SFTP
         $documents = Document::whereNull('copy_to_sftp')
             ->whereNotNull('path_pdf')
             ->whereNotNull('path_xml')
-            ->limit(20)
+            ->limit(100)
             ->get();
-        Log::info(" Vamos a copiar XML y PDF de  [" . $documents->count() . "] Documentos");
+        Log::info("Vamos a copiar XML y PDF de [" . $documents->count() . "] Documentos");
+
         foreach ($documents as $document) {
             $vv = Document::find($document->id)->copy_to_sftp;
             if ($vv != null) {
-                Log::info("Otro proceso ya realizo la copia a SFTP[" . $document->Number . "]");
+                Log::info("Otro proceso ya realizó la copia a SFTP[" . $document->Number . "]");
                 continue;
             }
-            //debemos pasar el pdf y el xml
+
+            // Debes pasar el pdf y el xml
             $pdf = '/app/' . $document->path_pdf;
             $xml = '/app/' . $document->path_xml;
 
             Log::debug("Buscando files [" . $pdf . "] xml [" . $xml . "]");
             if ($this->verificarArchivo($pdf) && $this->verificarArchivo($xml)) {
-                //Vamos a pasar el PDF
-
                 $valida = 0;
 
                 try {
-                    $vv = Document::find($document->id)->copy_to_sftp;
-                    if ($vv != null) {
-                        Log::info("Otro proceso ya realizo la copia a SFTP[" . $document->Number . "]");
+                    if (!$this->copiarArchivoPorSFTP($sftp, $pdf)) {
                         continue;
-                    } else {
-
-                        $a = $this->copiarArchivoPorSFTP($pdf);
-                        if ($a) {
-                            $document->save();
-                            $evento = new Evento();
-                            $evento->fecha_evento = Carbon::now()->format('Y-m-d H:i:s');
-                            $evento->observacion = "PDF Copiado a Servidor SFTP";
-                            $document->eventos()->save($evento);
-                            Log::debug("PDF Copiado SFTP: " . $document->Number);
-                            $valida = $valida + 1;
-                        }
                     }
+                    $document->save();
+                    $this->registerEvent($document, "PDF Copiado a Servidor SFTP");
+                    $valida++;
                 } catch (\Exception $e) {
-                    Log::error("Error al Copiar a SFTP PDF [" . $e->getTrace()."]");
-                    continue;
+                    Log::error("Error al Copiar a SFTP PDF [" . $e->getMessage() . "]");
                 }
 
-
-
                 try {
-                    $vv = Document::find($document->id)->copy_to_sftp;
-                    if ($vv != null) {
-                        Log::info("Otro proceso ya realizo la copia a SFTP[" . $document->Number . "]");
+                    if (!$this->copiarArchivoPorSFTP($sftp, $xml)) {
                         continue;
-                    } else {
-                        $a = $this->copiarArchivoPorSFTP($xml);
-                        if ($a) {
-                            $document->save();
-                            $evento = new Evento();
-                            $evento->fecha_evento = Carbon::now()->format('Y-m-d H:i:s');
-                            $evento->observacion = "XML Copiado a Servidor SFTP";
-                            $document->eventos()->save($evento);
-                            Log::info("XML Copiado SFTP: " . $document->Number);
-                            $valida = $valida + 1;
-                        }
                     }
+                    $document->save();
+                    $this->registerEvent($document, "XML Copiado a Servidor SFTP");
+                    $valida++;
                 } catch (\Exception $e) {
                     Log::error("Error al Copiar a SFTP XML" . $e->getMessage());
+                }
+
+                if ($valida == 2) {
+                    $document->copy_to_sftp = true;
                     $document->save();
-                    continue;
                 }
-
-                try {
-                    Log::info("valida : " . $valida);
-                    if ($valida == 2) {
-                        $document->copy_to_sftp = true;
-                        $document->save();
-                    }
-                } catch (\Exception $e) {
-                    Log::error("" . $e->getMessage());
-                    continue;
-                }
-
             } else {
                 Log::error("No existen ambos documentos para pasar a SFTP");
             }
-
-
-
-
-
         }
 
-
-
+        // Cerrar la conexión SFTP al final del proceso
+        $sftp->disconnect();
     }
 
+    private function establishSFTPConnection()
+    {
+        $host = env('SFTP_HOSTNAME');
+        $port = env('SFTP_PORT');
+        $user = env('SFTP_USERNAME');
+        $password = env('SFTP_PASSWORD');
 
-    public function verificarArchivo($rutaArchivo)
+        try {
+            $sftp = new SFTP($host, $port);
+            if (!$sftp->login($user, $password)) {
+                Log::error('USER O PASS FALLIDA PARA CONECTAR A SFTP');
+                throw new \Exception('Autenticación fallida para SFTP');
+            }
+            return $sftp;
+        } catch (\Exception $e) {
+            Log::error('Error al establecer la conexión SFTP: ' . $e->getMessage());
+            throw $e;
+        }
+    }
+
+    private function verificarArchivo($rutaArchivo)
     {
         $filesystem = new Filesystem();
 
@@ -147,77 +126,69 @@ class CopyToSFTP extends Command
         }
     }
 
-
-    public function copiarArchivoPorSFTP(string $archivo)
+    private function copiarArchivoPorSFTP($sftp, string $archivo)
     {
-        Log::info('Vamos a pasar por SFTP el archivo: ' . $archivo);
+	    Log::info('Vamos a pasar por SFTP el archivo: ' . $archivo);
 
-        $host = env('SFTP_HOSTNAME');
-        $user = env('SFTP_USERNAME');
-        $port = env('SFTP_PORT');
-        $password = env('SFTP_PASSWORD');
-        $path = env('SFTP_PATH');
-        Log::info('Host: [' . $host . '] User: [' . $user . '] Port: [' . $port . ']');
+	          $host = env('SFTP_HOSTNAME');
+	          $user = env('SFTP_USERNAME');
+	          $port = env('SFTP_PORT');
+	          $password = env('SFTP_PASSWORD');
+	          $path = env('SFTP_PATH');
+	          Log::info('Host: [' . $host . '] User: [' . $user . '] Port: [' . $port . ']');
 
-        Log::info("esto llego: " . $archivo);
-        $rutaArchivoLocal = storage_path("{$archivo}");
-        Log::info("este es el path completo: " . $rutaArchivoLocal);
-        $rutaArchivoRemoto = '/files/' . basename($archivo);
-
-
-        Log::info('archivo remoto: ' . $rutaArchivoRemoto);
+	          Log::info("esto llego: " . $archivo);
+	          $rutaArchivoLocal = storage_path("{$archivo}");
+	          Log::info("este es el path completo: " . $rutaArchivoLocal);
+	          $rutaArchivoRemoto = '/files/' . basename($archivo);
 
 
-        if (!file_exists($rutaArchivoLocal)) {
-            Log::error('El archivo local no existe en la ruta especificada: ' . $rutaArchivoLocal);
-            // Manejo del error aquí
-            return false; // Por ejemplo, puedes retornar false si el archivo local no existe
-        }
+	          Log::info('archivo remoto: ' . $rutaArchivoRemoto);
 
-        try {
 
-            // Crear una instancia de SFTP
-            $sftp = new SFTP($host, $port);
+	          if (!file_exists($rutaArchivoLocal)) {
+	              Log::error('El archivo local no existe en la ruta especificada: ' . $rutaArchivoLocal);
+	              // Manejo del error aquí
+	              return false; // Por ejemplo, puedes retornar false si el archivo local no existe
+	          }
 
-            // Autenticarse con usuario y contraseña
-            if (!$sftp->login($user, $password)) {
-                Log::error('USER O PASS FALLIDA PARA CONECTAR A SFTP');
-                $sftp->disconnect();
-                return false; // Si la autenticación falla, puedes retornar false aquí
-            }
+	          try {
 
-            // Verificar si el archivo remoto ya existe
-            if ($sftp->file_exists($rutaArchivoRemoto)) {
-                Log::error('El archivo remoto ya existe: ' . $rutaArchivoRemoto);
-                $sftp->disconnect();
-                return true; // Si el archivo remoto ya existe, puedes retornar false aquí
-            }
+	             
 
-            try {
-                if (!$sftp->put($rutaArchivoRemoto, $rutaArchivoLocal, SFTP::SOURCE_LOCAL_FILE)) {
-                    $sftp->disconnect();
-                    Log::error('Error al pasar el archivo: ' . $sftp->getLastSFTPError());
+	              // Verificar si el archivo remoto ya existe
+	              if ($sftp->file_exists($rutaArchivoRemoto)) {
+	                  Log::error('El archivo remoto ya existe: ' . $rutaArchivoRemoto);
+	                  return true; // Si el archivo remoto ya existe, puedes retornar false aquí
+	              }
+
+	              try {
+	                  if (!$sftp->put($rutaArchivoRemoto, $rutaArchivoLocal, SFTP::SOURCE_LOCAL_FILE)) {
+	                      Log::error('Error al pasar el archivo: ' . $sftp->getLastSFTPError());
                     
-                    return false;
-                }
-            } catch (\Exception $e) {
-                $sftp->disconnect();
-                Log::error('Excepción al pasar el archivo: ' . $e->getMessage());
+	                      return false;
+	                  }
+	              } catch (\Exception $e) {
+	                  Log::error('Excepción al pasar el archivo: ' . $e->getMessage());
                 
-                return false;
-            }
+	                  return false;
+	              }
 
-            // Cerrar la conexión SFTP
-            $sftp->disconnect();
+	        
+	              Log::info('Copiado Exitosamente' . $rutaArchivoRemoto);
+	              return true; // Indicar que la copia fue exitosa
 
-            Log::info('Copiado Exitosamente' . $rutaArchivoRemoto);
-            return true; // Indicar que la copia fue exitosa
-
-        } catch (\Exception $e) {
-            Log::error('Ocurrio un error conectando: [' . $e->getTrace() . "]");
-            $sftp->disconnect();
-            return false;
-        }
+	          } catch (\Exception $e) {
+	              Log::error('Ocurrio un error conectando: [' . $e->getTrace() . "]");
+	              return false;
+	          }
     }
 
+    private function registerEvent($document, $message)
+    {
+        $evento = new Evento();
+        $evento->fecha_evento = Carbon::now()->format('Y-m-d H:i:s');
+        $evento->observacion = $message;
+        $document->eventos()->save($evento);
+    }
 }
