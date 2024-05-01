f98c5eacb7cd86901842d9d24cf2a7868f86934a
lramirez <lramirez@3htp.com>
2024-05-01 01:20:06 -0400

    format fecha

--- resources/js/component/Monitor.vue
+++ resources/js/component/Monitor.vue
@@ -15,7 +15,7 @@
             <img v-else src="img/error.png" alt="Error" width="40" style="display: block; margin: 0 auto;">
           </td>
           <td>{{ item.componente }}</td>
-          <td>{{ item.last_check }}</td>
+          <td>{{ formatLastCheck(item.last_check) }}</td>
           <td>{{ item.observacion }}</td>
         </tr>
       </tbody>
@@ -35,8 +35,22 @@ export default {
   setup(props) {
     const data = ref(props.data);
 
+    function formatLastCheck(dateString) {
+      const date = new Date(dateString);
+      const formattedDate = date.toLocaleDateString('es-ES', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      });
+      return formattedDate;
+    }
+
     return {
-      data
+      data,
+      formatLastCheck
     };
   }
 };
